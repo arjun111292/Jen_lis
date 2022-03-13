d4efe2e6adf13e2c3d90ce677a7cf5f27d503467
shamil_lisa <97886263+shamil-lol@users.noreply.github.com>
2022-03-13 12:13:00 +0530

    Update spam.py

--- plugins/t_4_pgs/spam.py
+++ plugins/t_4_pgs/spam.py
@@ -1,1 +1,5 @@
+from pyrogram import Client, filters
+from info import COMMAND_HAND_LER
+from plugins.neededshits.cust_p_filters import f_onw_fliter
 
+@bot.on_message(filters.command('spam', COMMAND_HAND_LER) & filters.f_onw_filter)
