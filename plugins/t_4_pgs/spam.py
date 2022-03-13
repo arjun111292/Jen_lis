4bac309bd32c20d895ccd120188a30b36180e016
shamil_lisa <97886263+shamil-lol@users.noreply.github.com>
2022-03-13 12:56:03 +0530

    Update spam.py

--- plugins/t_4_pgs/spam.py
+++ plugins/t_4_pgs/spam.py
@@ -3,3 +3,13 @@ from info import COMMAND_HAND_LER
 from plugins.neededshits.cust_p_filters import f_onw_fliter
 
 @bot.on_message(filters.command('spam', COMMAND_HAND_LER) & filters.f_onw_filter)
+async def spam (cli, m):
+  time = 0
+  count = 100
+  cunt = 0
+  while True:
+    cunt += 1
+    await m.reply_text(f"spam {cunt}")
+    time += 1
+    if time >= count:
+      break
