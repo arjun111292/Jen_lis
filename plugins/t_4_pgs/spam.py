2fd829730ae6d9c7698bf5435b1c14dfd0c3439f
shamil_lisa <97886263+shamil-lol@users.noreply.github.com>
2022-03-13 12:56:26 +0530

    Update spam.py

--- plugins/t_4_pgs/spam.py
+++ plugins/t_4_pgs/spam.py
@@ -2,7 +2,7 @@ from pyrogram import Client, filters
 from info import COMMAND_HAND_LER
 from plugins.neededshits.cust_p_filters import f_onw_fliter
 
-@bot.on_message(filters.command('spam', COMMAND_HAND_LER) & filters.f_onw_filter)
+@Client.on_message(filters.command('spam', COMMAND_HAND_LER) & filters.f_onw_filter)
 async def spam (cli, m):
   time = 0
   count = 100
